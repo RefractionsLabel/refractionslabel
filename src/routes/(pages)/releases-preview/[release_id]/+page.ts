7234585bdf826e84ccd02d02d65b0f43fe1fa279
RefractionsLabel <joe@refractionslabel.com>
2026-02-04 16:49:49 +0000

    Delete src/routes/(pages) directory

--- src/routes/(pages)/releases-preview/[release_id]/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ params }) => {
-	const md = await import(`$lib/_content/releases/${params.release_id}.md`);
-	//const text = md.markdown;
-	const attributes = md.attributes;
-	return { attributes };
-};
