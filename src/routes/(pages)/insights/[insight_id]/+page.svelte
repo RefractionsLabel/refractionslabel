48d9e02ebee313d56b09a8addf8d2314386b9d80
Ronan Hoogmoed <ronanhoogmoed@hotmail.com>
2026-02-22 21:14:22 +0100

    add remaining style elements

--- src/routes/(pages)/insights/[insight_id]/+page.svelte
+++ src/routes/(pages)/insights/[insight_id]/+page.svelte
@@ -82,10 +82,45 @@
 		list-style-type: inherit;
 		list-style-position: inherit;
 	}
+	/* Nested lists: left margin offsets the block; padding for bullet space */
+	:global(.article ul ul),
+	:global(.article ol ul) {
+		margin-top: 0.25rem;
+		margin-bottom: 0;
+		margin-left: 1.5rem;
+		padding-left: 2rem;
+		list-style-type: circle;
+		list-style-position: inside;
+	}
+	:global(.article ul ul ul),
+	:global(.article ol ul ul),
+	:global(.article ol ol ul) {
+		list-style-type: square;
+		margin-left: 1.5rem;
+		padding-left: 2rem;
+	}
+	:global(.article ol ol),
+	:global(.article ul ol) {
+		margin-top: 0.25rem;
+		margin-bottom: 0;
+		margin-left: 1.5rem;
+		padding-left: 2rem;
+		list-style-type: lower-alpha;
+		list-style-position: inside;
+	}
+	:global(.article ol ol ol),
+	:global(.article ul ol ol) {
+		list-style-type: lower-roman;
+		margin-left: 1.5rem;
+		padding-left: 2rem;
+	}
 
 	/* Consistent vertical spacing between ALL blocks in the article */
 	:global(.article > * + *) {
 		margin-top: 1.5rem;
+	}
+	:global(.article li ul li) {
+		margin-left: 24px;
 	}
 
 	/* Headings in article: align with app.css h1/h2/h3 */
@@ -114,11 +149,23 @@
 		}
 	}
 	:global(.article h3) {
-		font-size: var(--text-lg);
+		font-size: var(--text-l);
+		margin: 0;
+	}
+	:global(.article h4) {
+		font-size: var(--text-ml);
+		margin: 0;
+	}
+	:global(.article h5) {
+		font-size: var(--text-sm);
+		margin: 0;
+	}
+	:global(.article h6) {
+		font-size: var(--text-xs);
 		margin: 0;
 	}
 
-	/* Bold text: variable font needs wght axis; same size as surrounding text */
+	/* Bold: variable font wght axis */
 	:global(.article strong) {
 		font-weight: 700;
 		font-variation-settings:
@@ -126,8 +173,115 @@
 			'wdth' 100;
 		font-size: inherit !important;
 	}
+	/* Italic */
+	:global(.article em) {
+		font-style: italic;
+		font-size: inherit !important;
+	}
+	/* Strikethrough (GFM) */
+	:global(.article del) {
+		text-decoration: line-through;
+		font-size: inherit !important;
+	}
+	/* Underline (HTML) */
+	:global(.article u) {
+		text-decoration: underline;
+		font-size: inherit !important;
+	}
+	/* Superscript / subscript */
+	:global(.article sup),
+	:global(.article sub) {
+		font-size: 0.75em;
+		line-height: 0;
+		vertical-align: baseline;
+	}
+	:global(.article sup) {
+		vertical-align: super;
+	}
+	:global(.article sub) {
+		vertical-align: sub;
+	}
 
-	/* Links in article */
+	/* Blockquote */
+	:global(.article blockquote) {
+		margin: 1rem 0;
+		padding-left: 1.5rem;
+		border-left: 4px solid rgb(3, 24, 3);
+		opacity: 0.95;
+		font-size: inherit !important;
+	}
+	:global(.article blockquote p) {
+		margin: 0.25rem 0;
+	}
+
+	/* Inline code */
+	:global(.article code) {
+		font-family: ui-monospace, monospace;
+		font-size: 0.9em;
+		padding: 0.2em 0.4em;
+		background: rgb(0 0 0 / 0.06);
+		border-radius: 4px;
+	}
+	/* Code block (pre > code) */
+	:global(.article pre) {
+		margin: 1rem 0;
+		padding: 1rem 1.25rem;
+		background: rgb(0 0 0 / 0.06);
+		border-radius: 4px;
+		overflow-x: auto;
+	}
+	:global(.article pre code) {
+		padding: 0;
+		background: none;
+		font-size: 0.85rem;
+		line-height: 1.5;
+	}
+
+	/* Tables */
+	:global(.article table) {
+		width: 100%;
+		margin: 1rem 0;
+		border-collapse: collapse;
+		font-size: 1rem;
+	}
+	:global(.article th),
+	:global(.article td) {
+		padding: 0.5rem 0.75rem;
+		border: 1px solid rgb(0 0 0 / 0.15);
+		text-align: left;
+	}
+	:global(.article th) {
+		font-weight: 700;
+		font-variation-settings:
+			'wght' 700,
+			'wdth' 100;
+		background: rgb(0 0 0 / 0.05);
+	}
+
+	/* Horizontal rule */
+	:global(.article hr) {
+		margin: 1.5rem 0;
+		border: none;
+		border-top: 2px solid rgb(0 0 0 / 0.2);
+	}
+
+	/* Task list (GFM): li contains <input type="checkbox"> + text */
+	:global(.article input[type='checkbox']) {
+		margin: 0 0.5rem 0 0;
+		vertical-align: middle;
+	}
+	:global(.article li:has(input[type='checkbox'])) {
+		list-style: none;
+	}
+
+	/* Images */
+	:global(.article img) {
+		max-width: 100%;
+		height: auto;
+		vertical-align: middle;
+	}
+
+	/* Links */
 	:global(.article a) {
 		text-decoration: underline;
 		color: inherit;
